1ae0e6c14b0b782a10b8a7a9877f6a04a5283fb4
hungry-345 <135878584+hungry-345@users.noreply.github.com>
2026-02-01 17:02:38 +0900

    2/1：17

---
+++ Assets/Mydata/Title/GameStartButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("GameSample 1");
+    }
+}
+
