45f9e23bd9cd7b55bef553f0b5a27fa8551067cc
hungry-345 <135878584+hungry-345@users.noreply.github.com>
2026-02-09 09:50:21 +0900

    2/9：9

---
+++ Assets/Mydata/How to play/NEXT_KeyBora.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NEXT_KeyBora : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("How to play 2");
+    }
+}
